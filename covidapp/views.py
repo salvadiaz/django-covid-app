66a088c9b458370fc597417e39aa7ef00c023d5a
jsdiaz219 <salvador.diaz@avec.com.ar>
2020-12-21 00:11:06 -0300

    covid api included

--- covidapp/views.py
+++ covidapp/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+import requests
+
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+    'x-rapidapi-key': "1661f2fde0mshbb6c45b45fdd8b9p1b4c21jsn490fd0c42abd",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers).json()
 
 
 # Create your views here.
 def hello_world_view(request):
-    return render(request, 'helloworld.html')
+    if request.method == 'POST':
+        selected_country = request.POST['selected_country']
+        print(selected_country)
+
+    results = int(response['results'])
+    my_list = []
+    for i in range(0, results):
+        my_list.append(response['response'][i]['country'])
+        context = {'my_list': my_list}
+    return render(request, 'helloworld.html', context)
