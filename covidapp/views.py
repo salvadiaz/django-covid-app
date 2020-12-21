f23b46bf7d4ad9b130dfeda4c395ad8e5feae3e2
jsdiaz219 <salvador.diaz@avec.com.ar>
2020-12-21 00:53:43 -0300

    backend ready

--- covidapp/views.py
+++ covidapp/views.py
@@ -13,13 +13,36 @@ response = requests.request("GET", url, headers=headers).json()
 
 # Create your views here.
 def hello_world_view(request):
-    if request.method == 'POST':
-        selected_country = request.POST['selected_country']
-        print(selected_country)
-
     results = int(response['results'])
     my_list = []
     for i in range(0, results):
         my_list.append(response['response'][i]['country'])
         context = {'my_list': my_list}
+
+    my_list.sort()
+    if request.method == 'POST':
+        selected_country = request.POST['selected_country']
+        print(selected_country)
+
+        for x in range(0, results):
+            if selected_country == response['response'][x]['country']:
+                new = response['response'][x]['cases']['new']
+                active = response['response'][x]['cases']['active']
+                critical = response['response'][x]['cases']['critical']
+                recovered = response['response'][x]['cases']['recovered']
+                total = response['response'][x]['cases']['total']
+                deaths = int(total) - int(active) - int(recovered)
+                print(response['response'][x]['cases']['new'])
+                context = {
+                    'selected_country': selected_country,
+                    'my_list': my_list,
+                    'new': new,
+                    'active': active,
+                    'critical': critical,
+                    'recovered': recovered,
+                    'total': total,
+                    'deaths': deaths
+                }
+                return render(request, 'helloworld.html', context)
+
     return render(request, 'helloworld.html', context)
